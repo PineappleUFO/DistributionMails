ce08d8c028fd120f4f89425b3446f422a1d98872
Дмитрий З <dimastrp@gmail.com>
2023-05-14 14:38:28 +0300

    Добавил цвета и стили

--- UI/AppShell.xaml.cs
+++ UI/AppShell.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace UI
+﻿using UI.Views.Components;
+
+namespace UI
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute("TestPage", typeof(Test));
+             
         }
     }
 }
