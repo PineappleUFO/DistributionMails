62c79d1e72cb90e4fc2a724ab2f95388effe1fe2
Дмитрий З <dimastrp@gmail.com>
2023-05-17 21:28:14 +0300

    Базовое окно "о письме" и навигация на него

--- UI/Views/Pages/MainForms/Input/InputMailMainViewModel.cs
+++ UI/Views/Pages/MainForms/Input/InputMailMainViewModel.cs
@@ -2,30 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using UI.Views.Pages.Message;
 
 namespace UI.Views.Pages.MainForms.Input
 {
-    public class MailModel
-    {
-        [DisplayName("Номер письма")]
-        public string NumberMail { get; set; }
-        [DisplayName("Дата получения")]
-        public DateTime DateInput { get; set; }
-        [DisplayName("Проект")]
-        public string Project { get; set; }
-        [DisplayName("Отправитель")]
-        public string Sender { get; set; }
-        [DisplayName("Тема")]
-        public string Theme { get; set; }
-        [DisplayName("Номер исх.")]
-        public string NumberOut { get; set; }
-        [DisplayName("Срок ответа")]
-        public DateTime DateOut { get; set; }
-    }
     [INotifyPropertyChanged]
     public partial class InputMailMainViewModel
     {
@@ -35,22 +19,33 @@ namespace UI.Views.Pages.MainForms.Input
         [ObservableProperty]
         public MailModel selectedMail;
 
+        [RelayCommand]
+        public async void OpenMailCommand(MailModel mail)
+        {
+            await Shell.Current.GoToAsync($"{nameof(MessageView)}",new Dictionary<string, object>()
+            {
+                ["SelectedMail"] = mail
+            });
+        }
+
         public InputMailMainViewModel()
         {
             listSourceMail = new ObservableCollection<MailModel>();
 
-            var m = new MailModel()
-            {
-                DateInput = DateTime.Now.AddDays(-4),
-                DateOut = DateTime.Now.AddDays(-2),
-                NumberMail = "1001",
-                NumberOut = "10-0",
-                Project = "project",
-                Sender = "sender",
-                Theme = "trheme"
-            };
+          
             for (int i = 0; i < 5; i++)
             {
+                var m = new MailModel()
+                {
+                    DateInput = DateTime.Now.AddDays(-4),
+                    DateOut = DateTime.Now.AddDays(-2),
+                    NumberMail = "1001",
+                    NumberOut = "10-0",
+                    Project = "project",
+                    Sender = "sender",
+                    Theme = "trheme",
+                    IdMail = i
+                };
                 listSourceMail.Add(m);
             }
         }
---
+++ UI/Views/Pages/Message/MessageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using UI.Views.Pages.MainForms.Input;
+
+namespace UI.Views.Pages.Message;
+
+[QueryProperty("SelectedMail","SelectedMail")]
+public partial class MessageViewModel : ObservableObject
+{ 
+    [ObservableProperty]
+    MailModel selectedMail;
+
+    public MessageViewModel()
+    {
+        
+    }
+}
