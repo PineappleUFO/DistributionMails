0d2ba5a6c5883d1c85135445953e1fc932033c99
Дмитрий З <dimastrp@gmail.com>
2023-05-21 16:12:20 +0300

    Вспомогательные методы для работы с pdf

--- UI/Helpers/PdfFileHelper.cs
+++ UI/Helpers/PdfFileHelper.cs
@@ -1,6 +1,27 @@
-﻿namespace UI.Helpers;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
 
-public class PdfFileHelper
+namespace UI.Helpers;
+
+public static class PdfFileHelper
 {
-    
+    public static int GetPageCount(string filePath)
+    {
+        using var reader = new PdfReader(filePath);
+        return reader.NumberOfPages;
+    }
+
+    public static byte[] GetPageContent(string filePath, int pageNumber)
+    {
+        using var reader = new PdfReader(filePath);
+        using var document = new Document(reader.GetPageSizeWithRotation(pageNumber));
+        using var stream = new MemoryStream();
+        var writer = PdfWriter.GetInstance(document, stream);
+        document.Open();
+        var importedPage = writer.GetImportedPage(reader, pageNumber);
+        var contentByte = writer.DirectContent;
+        contentByte.AddTemplate(importedPage, 0, 0);
+        document.Close();
+        return stream.ToArray();
+    }
 }
