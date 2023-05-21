3ee950f1b904093f6a5982224b5dfef5ba07c3de
Дмитрий З <dimastrp@gmail.com>
2023-05-21 16:12:00 +0300

    Добавил методы расширения для файлов

---
+++ UI/Extenstions/FileExtenstions.cs
@@ -0,0 +1,36 @@
+﻿namespace UI.Extenstions
+{
+    /// <summary>
+    /// Расширение методов
+    /// </summary>
+    public static class FileExtenstions
+    {
+        /// <summary>
+        /// Получить все PDF в папке
+        /// </summary>
+        /// <param name="folder">Папка</param>
+        public static List<FileInfo> GetAllPdfInFolder(this DirectoryInfo folder)
+        {
+            var list = new List<FileInfo>();
+            foreach (FileInfo file in folder.GetFiles())
+            {
+                if (file.Extension.Equals(".pdf", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    list.Add(file);
+                }
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// Проверки на 
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static bool CheckFilePath(this string filePath)
+        {
+            if (filePath == null || string.IsNullOrWhiteSpace(filePath) || !new FileInfo(filePath).Exists) return false;
+            return true;
+        }
+    }
+}
