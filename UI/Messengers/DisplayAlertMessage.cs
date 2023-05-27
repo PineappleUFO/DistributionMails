e0e29625dba14d419ba32057b3a25941c1c58c59
Дмитрий З <dimastrp@gmail.com>
2023-05-27 21:14:56 +0300

    Реализация меседжера и передача окна с ошибкой в LoginPage.xaml

---
+++ UI/Messengers/DisplayAlertMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace UI.Messengers;
+
+public class DisplayAlertMessage
+{
+    public string Title { get; set; }
+    public string Message { get; set; }
+    public string Cancel { get; set; }
+}
--- UI/Views/Pages/Login/LoginPage.xaml.cs
+++ UI/Views/Pages/Login/LoginPage.xaml.cs
@@ -1,9 +1,22 @@
-namespace UI.Views.Pages;
+using CommunityToolkit.Mvvm.Messaging;
+using UI.Helpers;
+using UI.Messengers;
+
+namespace UI.Views.Pages.Login;
 
 public partial class LoginPage : ContentPage
 {
+	private readonly IMessenger _messenger;
 	public LoginPage()
 	{
 		InitializeComponent();
+		BindingContext = ServiceHelper.GetService<LoginViewModel>();
+		_messenger = ServiceHelper.GetService<IMessenger>();
+		_messenger.Register<DisplayAlertMessage>(this,OnDisplayAlertMessageRecieved);
+	}
+
+	private async void OnDisplayAlertMessageRecieved(object recipient, DisplayAlertMessage message)
+	{
+		await DisplayAlert(message.Title, message.Message, message.Cancel);
 	}
 }
