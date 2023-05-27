42c35d03b3a0f2850935bb69f4f5d665b5a09991
Дмитрий З <dimastrp@gmail.com>
2023-05-27 21:15:18 +0300

    Привязка к VM и добавление конвертера

---
+++ UI/Converters/InvertedBooleanConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace UI.Converters;
+
+public class InvertedBooleanConverter:IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool b) return !b;
+        return default;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return default;
+    }
+}
