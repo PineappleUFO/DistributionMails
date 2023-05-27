f5f010c165c6be11e3507b0c9cad3cc0032e9f92
Дмитрий З <dimastrp@gmail.com>
2023-05-27 18:39:50 +0300

    добавил id в конструктор User

--- Core/Models/User.cs
+++ Core/Models/User.cs
@@ -6,13 +6,14 @@ public class User:Entity
     public string Name { get; private set; }
     public string Surname { get; private set; }
     public string Login { get; private set; }
-    public byte[] Photo { get; private set; }
+    public byte[]? Photo { get; private set; }
     public string Phone { get; private set; }
     //public int DepartmentId { get; private set; }
     //public int PositionId { get; private set; }
     
-    public User(string family, string name, string surname, string login, byte[] photo, string phone)
+    public User(int id,string family, string name, string surname, string login, byte[]? photo, string phone)
     {
+        Id = id;
         Family = family;
         Name = name;
         Surname = surname;
