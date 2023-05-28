74a88856b7335b6a9be4fdbb5a90439a8989f194
Дмитрий З <dimastrp@gmail.com>
2023-05-28 18:37:25 +0300

    Мелкие правки + цвета проектов

---
+++ UI/Converters/StringToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Converters
+{
+    public class StringToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(value is string colorHex)
+            {
+               return Microsoft.Maui.Graphics.Color.Parse(colorHex);
+            }
+            return default;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return default;
+        }
+    }
+}
