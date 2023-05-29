76f62e42d95c4b29376073f40b5e8017376315af
Дмитрий З <dimastrp@gmail.com>
2023-05-29 21:48:43 +0300

    базовая логика для распределения

---
+++ UI/Helpers/DistributionItem.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Helpers
+{
+    /// <summary>
+    /// Обертка User в ObservableObject и добавления отслеживаемого свойства
+    /// </summary>
+    public partial class DistributionItem:ObservableObject
+    {
+        [ObservableProperty] public User user;
+        [ObservableProperty] public DateTime deadline;
+        [ObservableProperty] public string resolution;
+        [ObservableProperty] public bool isResponsible;
+        [ObservableProperty] public bool isReplying;
+        [ObservableProperty] public bool isSelected;
+        [ObservableProperty] public bool isChecked = false;
+    }
+}
--- UI/Views/Pages/Distribution/DistributionViewModel.cs
+++ UI/Views/Pages/Distribution/DistributionViewModel.cs
@@ -1,17 +1,62 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using Core.Models;
+using EF.Repositories;
+using UI.Helpers;
 using UI.Views.Pages.MainForms.Input;
+using Microsoft.Maui.ApplicationModel.Communication;
 
 namespace UI.Views.Pages.Distribution
 {
     [QueryProperty("SelectedMail", "SelectedMail")]
     public partial class DistributionViewModel : ObservableObject
     {
-        [ObservableProperty]
-        MailModel selectedMail;
+        [ObservableProperty] DistributionItem selectedUser;
+        [ObservableProperty] public bool isBusy;
+        [ObservableProperty] public ObservableCollection<DistributionItem> userSource = new();
+        [ObservableProperty] public ObservableCollection<DistributionItem> selectedUserSource = new();
+
+        [RelayCommand]
+        public async void GetAllUsers()
+        {
+            IsBusy = true;
+            var uRep = ServiceHelper.GetService<UserRepository>();
+            var users = await uRep.GetAllUsers();
+            foreach (var user in users.Where(user => user != null))
+            {
+                UserSource.Add(new DistributionItem() { User = user });
+            }
+            IsBusy = false;
+        }
+
+        [RelayCommand]
+        public async void Cancel()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+
+        [RelayCommand]
+        public void CheckUser(DistributionItem user)
+        {
+            if(user.IsChecked)
+            {
+                SelectedUserSource.Add(user);
+            }
+            else
+            {
+                SelectedUserSource.Remove(user);
+            }
+        }
+
+        public DistributionViewModel()
+        {
+            GetAllUsersCommand.Execute(null);
+        }
     }
 }
