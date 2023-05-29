8d399073658acc2140654b8d501a2e8e1166c373
Дмитрий З <dimastrp@gmail.com>
2023-05-29 21:48:22 +0300

    UI окна распределения

--- UI/Views/Pages/Distribution/DistributionPage.xaml.cs
+++ UI/Views/Pages/Distribution/DistributionPage.xaml.cs
@@ -7,6 +7,36 @@ public partial class DistributionPage : ContentPage
 	public DistributionPage()
 	{
 		InitializeComponent();
-		BindingContext = BindingContext = ServiceHelper.GetService<DistributionViewModel>();
+		BindingContext = ServiceHelper.GetService<DistributionViewModel>();
+    }
+
+    private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if(BindingContext is DistributionViewModel distributionViewModel)
+        {
+            distributionViewModel.CheckUser(dgUsers.SelectedItem as DistributionItem);
+        }
+    }
+
+    private void dgUsers_ItemSelected(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.Count == 0) return;
+
+        if (e.CurrentSelection[0] is DistributionItem currentSelection)
+        {
+            currentSelection.IsSelected = true;
+
+        }
+
+        if (e.PreviousSelection.Count == 0) return;
+
+        if (e.PreviousSelection[0] is DistributionItem prevSelection)
+        {
+            if(!prevSelection.IsChecked)
+            {
+                prevSelection.IsSelected = false;
+            }
+
+        }
     }
 }
