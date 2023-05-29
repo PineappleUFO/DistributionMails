f1e79fd462fe3d9ba66cc62c7aad3ad8689a5219
Дмитрий З <dimastrp@gmail.com>
2023-05-29 21:46:55 +0300

    Добавил новый метод "получить в всех пользователей" в репозиторий пользователей

--- PostgresRepository/Interfaces/IUserRepository.cs
+++ PostgresRepository/Interfaces/IUserRepository.cs
@@ -6,4 +6,5 @@ namespace PostgresRepository.Interfaces;
 public interface IUserRepository
 {
     Task<User?> TryGetUserByLogin(string login, string password,CancellationToken cancellationToken);
+    Task<List<User?>> GetAllUsers();
 }
--- PostgresRepository/Repositories/UserRepository.cs
+++ PostgresRepository/Repositories/UserRepository.cs
@@ -71,4 +71,67 @@ where u.login = '{login}' and
         //Если подключение не корректно 
         return null;
     }
+
+    public async Task<List<User?>> GetAllUsers()
+    {
+        
+        //если по какой то причине строка подключения пустая
+        if (string.IsNullOrWhiteSpace(PostgresConnectionString.ConnectionString))
+            throw new Exception("Не задана строка подключения");
+
+        await using var connection = new NpgsqlConnection(PostgresConnectionString.ConnectionString);
+        await connection.OpenAsync();
+
+        List<User> result = new();
+        try
+        {
+            await using var command = connection.CreateCommand();
+            command.CommandText =
+                $@"select u.user_id,
+       u.family,
+       u.name,
+       u.surname,
+       u.login,
+       u.photo,
+       u.phone,
+       d.dep_id,
+       d.dep_name,
+       d.dep_about,
+       p.position_id,
+       p.position_name,
+ (SELECT CONCAT(LEFT(u.name, 1), '.', LEFT(u.surname, 1),'.')) AS inicials
+from users u,deps d,positions p
+where u.id_dep = d.dep_id and
+      p.position_id = u.id_position";
+
+            await using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                var user =  new User(reader.GetInt32(0),
+                    reader.GetString(1),
+                    reader.GetString(2),
+                    reader.GetString(3),
+                    reader.GetString(4),
+                    null,
+                    reader.GetString(6),
+                    new Dep(reader.GetInt32(7),reader.GetString(8),reader.GetString(9)),
+                    new Position(reader.GetInt32(10),reader.GetString(11)));
+
+                user.Inicials = reader["inicials"].ToString();
+                result.Add(user);
+            }
+        }
+        catch (NpgsqlException e)
+        {
+            //todo: логирование
+            Console.WriteLine(e);
+            throw;
+        }
+        finally
+        {
+            await connection.CloseAsync();
+        }
+        
+        return result;
+    }
 }
