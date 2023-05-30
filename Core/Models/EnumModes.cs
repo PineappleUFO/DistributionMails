39da078d8b2cf13b0f1f3ca8491b96fc25b89727
Дмитрий З <dimastrp@gmail.com>
2023-05-30 21:55:29 +0300

    Добавил Template для выбора режимов

---
+++ Core/Models/EnumModes.cs
@@ -0,0 +1,13 @@
+﻿namespace Core.Models
+{
+    /// <summary>
+    /// Главные режимы отображения входящей переписки
+    /// </summary>
+    public enum EnumModes
+    {
+        Archive,
+        DistributedToMe,
+        All,
+        Favorite
+    }
+}
