9f0a7e2daeafa559e41d23833c232da8d17704e7
Дмитрий З <dimastrp@gmail.com>
2023-06-12 02:23:37 +0300

    добавил конф. файлы докер в github action

---
+++ Core.Test/DepModelTests.cs
@@ -0,0 +1,52 @@
+﻿using Core.Models;
+
+namespace Core.Test
+{
+    public class DepModelTests
+    {
+        [Fact]
+        public void Constructor_SetsPropertiesCorrectly()
+        {
+            // Arrange
+            int id = 1;
+            string name = "Department 1";
+            string about = "About Department 1";
+
+            // Act
+            Dep dep = new Dep(id, name, about);
+
+            // Assert
+            Assert.Equal(id, dep.Id);
+            Assert.Equal(name, dep.Name);
+            Assert.Equal(about, dep.About);
+        }
+
+        [Fact]
+        public void Name_Property_CanBeSetAndRetrieved()
+        {
+            // Arrange
+            string name = "Department 1";
+            Dep dep = new Dep(1, "", "");
+
+            // Act
+            dep.Name = name;
+
+            // Assert
+            Assert.Equal(name, dep.Name);
+        }
+
+        [Fact]
+        public void About_Property_CanBeSetAndRetrieved()
+        {
+            // Arrange
+            string about = "About Department 1";
+            Dep dep = new Dep(1, "", "");
+
+            // Act
+            dep.About = about;
+
+            // Assert
+            Assert.Equal(about, dep.About);
+        }
+    }
+}
